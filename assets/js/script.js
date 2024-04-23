763b9c3121a41d7b98d09a3fa009afc7b81d1eb5
Noah Manno <noahmanno19@gmail.com>
2024-04-23 11:42:03 -0400

    populate page

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,31 +6,30 @@ pokemonSubmitEl.on('click', handleSubmitPokemon)
 function handleSubmitPokemon() {
     // Get the User Input Pokemon
     let newPokemonSearch = pokemonSearchEl.val()
+    if (newPokemonSearch) {
     console.log(newPokemonSearch);
     // Grab the Search History From Local Storage
     let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
     // If it has not been searched before, add it to the search history and update storage
-    if (!searchHistory.includes(newPokemonSearch)) {
-        searchHistory.push(newPokemonSearch);
-        localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
-    }
-    handleFetchPokemonData(newPokemonSearch);
+        let url = `https://pokeapi.co/api/v2/pokemon/${newPokemonSearch}/`
+        fetch(url) 
+        .then(function (response) {
+            if (!response.ok) {
+                throw response.json();
+            }
+                return response.json();
+        })
+        .then(function(data) {
+            if (!searchHistory.includes(newPokemonSearch)) {
+                searchHistory.unshift(newPokemonSearch);
+                localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+            }
+            localStorage.setItem('currentPokemonData', JSON.stringify(data));
+            window.location.href = 'results.html'
+        })
+        .catch(function(error) {
+            console.error('Error fetching data:', error);
+        });
+}
 }
 
-function handleFetchPokemonData(pokemon) {
-    let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}/`
-    fetch(url) 
-    .then(function (response) {
-        if (!response.ok) {
-            throw response.json();
-        }
-            return response.json();
-    })
-    .then(function(data) {
-        localStorage.setItem('currentPokemonData', JSON.stringify(data));
-        window.location.href = 'results.html'
-    })
-    .catch(function(error) {
-        console.error('Error fetching data:', error);
-    });
-}
