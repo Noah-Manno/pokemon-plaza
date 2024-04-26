dc790f2b3a151f7a5ce50c4a2cb058dc17e11e20
Noah Manno <noahmanno19@gmail.com>
2024-04-25 20:08:40 -0400

    changed form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
 let pokemonSearchEl = $('#search')
 let pokemonSubmitEl = $('#submit')
-let searchForm = $('#searchForm')
+let searchForm = $('#search-form')
 // Grab the Search History From Local Storage
 let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
 
-pokemonSubmitEl.on('click touchstart', handleSubmitPokemon)
 
 searchForm.on('submit', function(event) {
     event.preventDefault(); // Prevent the default form submission behavior
